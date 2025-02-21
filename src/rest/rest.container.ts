8287965984c0018f76d310ba84aa143684d300b1
redx099 <ferz099@mail.ru>
2025-02-21 12:52:52 +0500

    Правки импортов

--- src/rest/rest.container.ts
+++ src/rest/rest.container.ts
@@ -1,17 +1,17 @@
 import { Container } from 'inversify';
 import { RestApplication } from './rest.application.js';
 import { Component } from '../shared/types/index.js';
-import { Logger, PinoLogger } from '../shared/libs/logger/index.js';
-import { Config, RestConfig, RestSchema } from '../shared/libs/config/index.js';
-import { DatabaseClient, MongoDatabaseClient } from '../shared/libs/database-client/index.js';
+import { ILogger, PinoLogger } from '../shared/libs/logger/index.js';
+import { IConfig, RestConfig, RestSchemaType } from '../shared/libs/config/index.js';
+import { IDatabaseClient, MongoDatabaseClient } from '../shared/libs/database-client/index.js';
 
 export function createRestApplicationContainer() {
   const restApplicationContainer = new Container();
 
   restApplicationContainer.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  restApplicationContainer.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  restApplicationContainer.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  restApplicationContainer.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  restApplicationContainer.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  restApplicationContainer.bind<IConfig<RestSchemaType>>(Component.Config).to(RestConfig).inSingletonScope();
+  restApplicationContainer.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   return restApplicationContainer;
 }
